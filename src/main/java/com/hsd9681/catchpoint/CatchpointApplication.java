5646f042494d77afd6a5d5e2a4ba05f1188a0061
hsd9681 <hsd9681@naver.com>
2025-01-31 21:45:20 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/hsd9681/catchpoint/CatchpointApplication.java
@@ -0,0 +1,13 @@
+package com.hsd9681.catchpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchpointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatchpointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hsd9681/catchpoint/CatchpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsd9681.catchpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
